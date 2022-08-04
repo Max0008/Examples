60103e7890d396aa3b628b4e241aea5d38644c7d
Max0008 <maximys0008@gmail.com>
2022-08-04 17:00:33 +0300

    added example005_conditionifelse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurraah, this is Masha!");
+}
+else 
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
